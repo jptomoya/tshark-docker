347950c622c13e6a1ad3a061eec733c8e5dbf23b
jptomoya <jptomoya@users.noreply.github.com>
2024-05-16 01:11:03 +0000

    Update the version of tshark to the latest version.

--- install-tshark.sh
+++ install-tshark.sh
@@ -6,12 +6,12 @@ dir_debs="$(mktemp -d)"
   cd "$dir_debs"
 
   # picked from latest Ubuntu APT Package: https://gitlab.com/wireshark/wireshark/-/pipelines?page=1&scope=tags&status=success
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/6493181831/artifacts/raw/ubuntu-packages/libwireshark-data_4.2.4_all.deb
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/6493181831/artifacts/raw/ubuntu-packages/libwsutil15_4.2.4_amd64.deb
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/6493181831/artifacts/raw/ubuntu-packages/libwiretap14_4.2.4_amd64.deb
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/6493181831/artifacts/raw/ubuntu-packages/libwireshark17_4.2.4_amd64.deb
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/6493181831/artifacts/raw/ubuntu-packages/wireshark-common_4.2.4_amd64.deb
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/6493181831/artifacts/raw/ubuntu-packages/tshark_4.2.4_amd64.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/6861199735/artifacts/raw/ubuntu-packages/libwireshark-data_4.2.5_all.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/6861199735/artifacts/raw/ubuntu-packages/libwsutil15_4.2.5_amd64.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/6861199735/artifacts/raw/ubuntu-packages/libwiretap14_4.2.5_amd64.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/6861199735/artifacts/raw/ubuntu-packages/libwireshark17_4.2.5_amd64.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/6861199735/artifacts/raw/ubuntu-packages/wireshark-common_4.2.5_amd64.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/6861199735/artifacts/raw/ubuntu-packages/tshark_4.2.5_amd64.deb
   echo "wireshark-common wireshark-common/install-setuid boolean true" | debconf-set-selections
   if ! (DEBIAN_FRONTEND=noninteractive dpkg -i -R . && :); then
     apt-get update
