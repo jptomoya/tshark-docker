6dd460d29a2560e0747d3d40959a351d919e8385
jptomoya <4786564+jptomoya@users.noreply.github.com>
2026-09-24 03:51:36 +0000

    Update the version of tshark to the latest version.

--- install-tshark.sh
+++ install-tshark.sh
@@ -6,12 +6,12 @@ dir_debs="$(mktemp -d)"
   cd "$dir_debs"
 
   # picked from latest Ubuntu APT Package: https://gitlab.com/wireshark/wireshark/-/pipelines?page=1&scope=tags&status=success
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/15863163337/artifacts/raw/ubuntu-packages/libwireshark-data_4.6.8_all.deb
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/15863163337/artifacts/raw/ubuntu-packages/libwsutil17_4.6.8_amd64.deb
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/15863163337/artifacts/raw/ubuntu-packages/libwiretap16_4.6.8_amd64.deb
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/15863163337/artifacts/raw/ubuntu-packages/libwireshark19_4.6.8_amd64.deb
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/15863163337/artifacts/raw/ubuntu-packages/wireshark-common_4.6.8_amd64.deb
-  wget https://gitlab.com/wireshark/wireshark/-/jobs/15863163337/artifacts/raw/ubuntu-packages/tshark_4.6.8_amd64.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/16689146385/artifacts/raw/ubuntu-packages/libwireshark-data_4.6.9_all.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/16689146385/artifacts/raw/ubuntu-packages/libwsutil17_4.6.9_amd64.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/16689146385/artifacts/raw/ubuntu-packages/libwiretap16_4.6.9_amd64.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/16689146385/artifacts/raw/ubuntu-packages/libwireshark19_4.6.9_amd64.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/16689146385/artifacts/raw/ubuntu-packages/wireshark-common_4.6.9_amd64.deb
+  wget https://gitlab.com/wireshark/wireshark/-/jobs/16689146385/artifacts/raw/ubuntu-packages/tshark_4.6.9_amd64.deb
   echo "wireshark-common wireshark-common/install-setuid boolean true" | debconf-set-selections
   if ! (DEBIAN_FRONTEND=noninteractive dpkg -i -R . && :); then
     apt-get update
